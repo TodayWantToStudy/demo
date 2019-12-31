e6e52e36918441bd9ccbff6ff4eab5687170e8ae
TodayWantToStudy <1019686614@qq.com>
2019-12-31 18:50:40 +0800

    add reflect server source code

--- client.cpp
+++ client.cpp
@@ -1,11 +1,16 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
 #include<arpa/inet.h>
 #include<unistd.h>
 #include<assert.h>
+#include<errno.h>
+void str_client(FILE*, int);
+size_t writen(int, const char*, size_t);
 
+const int MAXLINE = 1024;
 const int SETVER_PORT = 8890;
 int main(int argc, char** argv){
 	int sockfd;
@@ -17,29 +22,52 @@ int main(int argc, char** argv){
 
 	bzero(&server_addr, sizeof(server_addr));
 	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = inet_addr(argv[2]);
+	server_addr.sin_addr.s_addr = inet_addr(argv[1]);
 	server_addr.sin_port = htons(SETVER_PORT);
 
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	int ret = connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
 	assert(ret >= 0);
 
 	str_client(stdin, sockfd);
 	return 0;
 }
+
+
+void err_sys(const char*);
+void Writen(int, const char*, size_t);
+size_t writen(int, const char*, size_t);
+
+
+size_t my_read(int, char*);
+size_t readline(int, char*, size_t);
+
 void str_client(FILE* fp, int sockfd){
-	char sendline[1024], recvline[1024];
-	while(fgets(sendline, 1024, fp) != NULL){
-		write(sockfd, sendline, strlen(sendline));
-		read(sockfd, recvline, 1024);
+	size_t rc;
+	char sendline[MAXLINE], recvline[MAXLINE];
+	while(fgets(sendline, MAXLINE, fp) != NULL){
+		Writen(sockfd,sendline, strlen(sendline));
+		if(readline(sockfd, recvline, MAXLINE) < 0){
+			printf("readline error");
+			printf("errno %d", errno);
+			exit(1);
+		}
 		fputs(recvline, stdout);
 	}
 }
 
-size_t writen(int fd, const char* vptr, size_t n){
-	size_t nleft = n;
-	const char* ptr = vptr;
+void Writen(int fd, const char* ptr, size_t nbytes){
+	if(writen(fd, ptr, nbytes) != nbytes)
+		err_sys("writen error");
+}
 
+size_t writen(int fd, const char* vptr, size_t n){
+	size_t nleft;
 	size_t nwritten;
+	const char* ptr;
+
+	ptr = vptr;
+	nleft = n;
 	while(nleft > 0){
 		if( (nwritten = write(fd, ptr, nleft)) <= 0 ){
 			if(nwritten < 0 && errno == EINTR)
@@ -50,5 +78,55 @@ size_t writen(int fd, const char* vptr, size_t n){
 		nleft -= nwritten;
 		ptr += nwritten;
 	}
+	return n;
+}
+
+void err_sys(const char* fmt){
+	printf("%s\n", fmt);
+	printf("errno:%d\n", errno);
+	exit(1);
+}
+
+static int read_cnt;
+static char *read_ptr;
+static char read_buf[MAXLINE];
+size_t my_read(int fd, char* ptr){
+	if(read_cnt <= 0){
+	again:
+		if( (read_cnt = read(fd, read_buf, MAXLINE)) < 0 ){
+			if(errno == EINTR)
+				goto again;
+			return -1;
+		}
+		else if( read_cnt == 0 ){
+			return 0;
+		}
+		read_ptr = read_buf;
+	}
+	read_cnt--;
+	*ptr = *read_ptr++;
+	return 1;
+}
+size_t readline(int fd, char* vptr, size_t maxlen){
+	size_t n, rc;
+	char c, *ptr;
+
+	ptr = vptr;
+	for(int n=1; n<maxlen; n++){
+		if( (rc = my_read(fd, &c)) == 1 ){
+			*ptr++ = c;
+			if(c == '\n')
+				break;
+		}
+		else if(rc == 0){
+			*ptr = 0;
+			return n-1;
+		}
+		else{
+			return -1;
+		}
+	}
+
+	*ptr = 0;
 	return n;
 }
--- server.cpp
+++ server.cpp
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
 #include<sys/socket.h>
 #include<sys/sendfile.h>
@@ -6,22 +7,25 @@
 #include<fcntl.h>
 #include<unistd.h>
 #include<assert.h>
+#include<errno.h>
+void str_server(int);
 
+const int MAXLINE = 1024;
 const int SERVER_PORT = 8890;
 int main(int argc, char** argv){
-	int linstenfd;
+	int listenfd;
 	struct sockaddr_in server_address;
-2
+
 	bzero(&server_address, sizeof(server_address));
 	server_address.sin_family = AF_INET;
 	server_address.sin_addr.s_addr = htonl(INADDR_ANY);
 	server_address.sin_port = htons(SERVER_PORT);
 
-	linstenfd = socket(AF_INET, SOCK_STREAM, 0);
-	assert(linstenfd >= 0);
-	int ret = bind(linstenfd, (struct sockaddr*)&server_address, sizeof(server_address));
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+	assert(listenfd >= 0);
+	int ret = bind(listenfd, (struct sockaddr*)&server_address, sizeof(server_address));
 	assert(ret != -1);
-	ret = linstenfd(linstenfd, 5);
+	ret = listen(listenfd, 5);
 	assert(ret != -1);
 
 	int connfd;
@@ -29,46 +33,71 @@ int main(int argc, char** argv){
 	while(true){
 		struct sockaddr_in client_address;
 		socklen_t client_addrlength = sizeof(client_address);
-		connfd = accept(linstenfd, (struct sockaddr*)&client_address, &client_addrlength);
+		connfd = accept(listenfd, (struct sockaddr*)&client_address, &client_addrlength);
 		if(connfd < 0){
 			printf("accept error\n");
-			exit(1);
+			return 1;
 		}
 
 		childpid = fork();
 		if(fork < 0){
 			printf("fork error\n");
-			exit(2);
+			return 2;
 		}
 		else if(childpid == 0){
-			close(linstenfd);
-			str_echo(connfd);
-			exit(0);
+			close(listenfd);
+			str_server(connfd);
+			return 0;
 		}
 		close(connfd);
 	}
 }
 
-void str_echo(int connfd){
-	char buf[1024];
-	int len = read(connfd, buf, 1024);
-	if(len < 0){
-		printf("read error\n");
+void err_sys(const char*);
+void Writen(int, const char*, size_t);
+size_t writen(int, const char*, size_t);
+
+void str_server(int connfd){
+	size_t n;
+	char buf[MAXLINE];
+again:
+	while( (n = read(connfd, buf, MAXLINE)) > 0 ){
+		Writen(connfd, buf, n);
 	}
-	else if(len == 0){
-		printf("read nothing\n");
+
+	if(n < 0 && errno == EINTR)
+		goto again;
+	else if(n < 0)
+		err_sys("str_echo: read error");
+}
+
+void Writen(int fd, const char* ptr, size_t nbytes){
+	if(writen(fd, ptr, nbytes) != nbytes)
+		err_sys("writen error");
+}
+
+size_t writen(int fd, const char* vptr, size_t n){
+	size_t nleft;
+	size_t nwritten;
+	const char* ptr;
+
+	ptr = vptr;
+	nleft = n;
+	while(nleft > 0){
+		if( (nwritten = write(fd, ptr, nleft)) <= 0 ){
+			if(nwritten < 0 && errno == EINTR)
+				nwritten = 0;
+			else
+				return -1;
+		}
+		nleft -= nwritten;
+		ptr += nwritten;
 	}
-	else if(len > 0){
-		printf("read:\n%s\n", buf);
-		int len = write(connfd, buf, len);
-		if(len < 0){
-			printf("write error\n");
-		}
-		else if(len == 0){
-			printf("write nothing\n");
-		}
-		else if(len > 0){
-			printf("write succussful\n");
-		}
-	}
+	return n;
+}
+
+void err_sys(const char* fmt){
+	printf("%s\n", fmt);
+	printf("errno:%d\n", errno);
+	exit(1);
 }
